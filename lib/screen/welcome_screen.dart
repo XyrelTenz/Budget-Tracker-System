bf4acf08c97367a87447e61de7eec80ce90fea28
XyrelTenz <xdemocrito1@gmail.com>
2025-11-12 11:50:25 +0800

    Test

---
+++ lib/screen/welcome_screen.dart
@@ -0,0 +1,22 @@
+import "package:flutter/material.dart";
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Center(child: Text("Test"))),
+    );
+  }
+}
