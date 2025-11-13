72046a9da9208c189d4a15110bb8edb44ff2a248
XyrelTenz <xdemocrito1@gmail.com>
2025-11-13 14:10:51 +0800

    New Widgets

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,55 +1,38 @@
 import 'package:flutter/material.dart';
+import "package:budget_tracker/widget/cards_widget.dart";
 
-class Cards extends StatelessWidget {
-  const Cards({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.yellow[100],
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: SingleChildScrollView(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              GridView.count(
-                crossAxisCount: 1,
-                scrollDirection: Axis.horizontal,
-                primary: false,
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 5,
-                children: [
-                  SizedBox(
-                    height: 100,
-                    child: ListView.separated(
-                      scrollDirection: Axis.horizontal,
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      itemCount: 10,
-                      separatorBuilder: (context, index) =>
-                          const SizedBox(width: 10),
-                      itemBuilder: (context, index) => const Cards(),
-                    ),
+            children: <Widget>[
+              SizedBox(
+                height: 200,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 20,
                   ),
-                ],
+                  itemCount: 10,
+                  separatorBuilder: (_, __) => const SizedBox(width: 10),
+                  itemBuilder: (context, index) => const CardItem(),
+                ),
               ),
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class Card extends StatelessWidget {
-  const Card({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(5),
       ),
     );
   }
---
+++ lib/widget/cards_widget.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+class CardItem extends StatelessWidget {
+  const CardItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 250,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+  }
+}
