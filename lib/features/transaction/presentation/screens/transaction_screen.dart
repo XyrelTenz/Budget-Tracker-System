c8da766ebfbf0bf48bebd5d024952b0b5ec00818
XyrelTenz <xdemocrito1@gmail.com>
2025-11-23 00:16:14 +0800

    Move The SnackBar to shared

--- lib/features/transaction/presentation/screens/transaction_screen.dart
+++ lib/features/transaction/presentation/screens/transaction_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+
+//Widgets
 import "package:smart_budget_ph/features/transaction/data/transaction_model.dart";
 import "package:smart_budget_ph/features/transaction/presentation/widgets/transaction_item_tile.dart";
 import "package:smart_budget_ph/features/transaction/presentation/widgets/transaction_summary_card.dart";
-// 1. FIXED: Added the missing import for the SnackBar
 import "package:smart_budget_ph/features/transaction/presentation/widgets/add_modal.dart";
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
-import "package:smart_budget_ph/features/transaction/presentation/widgets/snack_bar_widgets.dart";
+import "package:smart_budget_ph/core/widgets/shared/snackbar_widget.dart";
 
 class TransactionScreen extends StatefulWidget {
   const TransactionScreen({super.key});
@@ -18,10 +19,9 @@ class _TransactionScreenState extends State<TransactionScreen> {
   String _searchQuery = "";
   String _selectedTab = "All";
 
-  // Load data
   final List<TransactionModel> _allTransactions = TransactionModel.dummyData;
 
-  // Logic to Filter Data
+  // Filter
   List<TransactionModel> get _filteredTransactions {
     return _allTransactions.where((tx) {
       if (_selectedTab != "All" && tx.type != _selectedTab) return false;
@@ -44,13 +44,11 @@ class _TransactionScreenState extends State<TransactionScreen> {
 
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () async {
-          // Use the package function instead of showModalBottomSheet
           final result = await showMaterialModalBottomSheet(
             context: context,
-            expand: false, // false = wrap content height; true = full screen
+            expand: false,
             enableDrag: true,
-            backgroundColor:
-                Colors.transparent, // Important: Lets your rounded corners show
+            backgroundColor: Colors.transparent,
             builder: (context) => const AddTransactionModal(),
           );
 
@@ -168,8 +166,7 @@ class _TransactionScreenState extends State<TransactionScreen> {
         hintText: "Search transactions...",
         prefixIcon: const Icon(Icons.search),
         filled: true,
-        // 3. FIXED: changed .withValues() to .withOpacity() for compatibility
-        fillColor: colorScheme.surfaceContainerHighest.withOpacity(0.5),
+        fillColor: colorScheme.surfaceContainerHighest.withValues(alpha: 0.5),
         contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(30),
